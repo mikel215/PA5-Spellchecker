8802fa0ceee606cffcaa90b3e468a36734e9b59b
mikel <mikelmayorga08@gmail.com>
2019-04-15 16:30:14 -0700

    added PriorityQueueAVL.cs

---
+++ PA5/PriorityQueueAVL.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA5
+{
+    // Implements list based
+    class PriorityQueueAVL
+    {
+        //Fields
+        private WordNode _root;
+        private int size;
+
+        // Constructor
+        public PriorityQueueAVL()
+        {
+            _root = null;
+            size = 0;
+        }
+
+        // Methods
+        public void Insert(WordNode node)
+        {
+            // if empty
+            if(_root == null)
+            {
+                _root = node;
+                return;
+            }
+            // insert element into AVL tree, balances as elements are added
+            _root.AddElement(_root, node);
+            size++;
+
+            // verify if it is a min heap
+            for(int i = 0; i < size; i++)
+            {
+                MakeHeap(ref _priorityQueue, size, i);
+            }
+            return;
+
+        }
+
+        public WordNode Pop()
+        {
+            WordNode smallest = _priorityQueue[0];
+
+            _priorityQueue.RemoveAt(0);
+            int size = _priorityQueue.Count;
+
+            for(int i = 0; i < size; i++ )
+            {
+                MakeHeap(ref _priorityQueue, size, i);
+            }
+
+            return smallest;
+        }
+
+        private void MakeHeap(ref WordNode node)
+        {
+            // root = i
+            WordNode smallest = node;
+            WordNode left = node.LeftChild;
+            WordNode right = node.RightChild;
+
+            // if left is smaller than root
+            if (left < smallest)
+            {
+                smallest = left;
+            }
+            //if right is smaller than root
+            if (right < smallest)
+            {
+                smallest = right;
+            }
+            // swap nodes
+            if (smallest != node)
+            {
+                WordNode temp = node;
+                node = smallest;
+                smallest = temp;
+
+                MakeHeap(ref smallest);
+            }
+        }
+        private bool IsMinHeap(List<WordNode> pq)
+        {
+            int size = pq.Count;
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (pq[i] < pq[(i - 1) / 2])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ PA5/PriorityQueueVector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA5
+{
+    // Implements list based
+    class PriorityQueue
+    {
+        //Fields
+        private List<WordNode> _priorityQueue;
+
+        // Constructor
+        public PriorityQueue()
+        {
+            _priorityQueue = new List<WordNode>();
+        }
+
+        // Methods
+        public void Insert(WordNode node)
+        {
+            int size = _priorityQueue.Count;
+            
+            // empty pq
+            if(size == 0)
+            {
+                _priorityQueue.Add(node);
+                return;
+            }
+
+            _priorityQueue.Add(node);
+            for(int i = 0; i < size; i++)
+            {
+                MakeHeap(ref _priorityQueue, size, i);
+            }
+            return;
+
+        }
+
+        public WordNode Pop()
+        {
+            WordNode smallest = _priorityQueue[0];
+
+            _priorityQueue.RemoveAt(0);
+            int size = _priorityQueue.Count;
+
+            for(int i = 0; i < size; i++ )
+            {
+                MakeHeap(ref _priorityQueue, size, i);
+            }
+
+            return smallest;
+        }
+
+        private void MakeHeap(ref List<WordNode> pq, int size, int index)
+        {
+            // root = i
+            int smallest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+
+            // if left is smaller than root
+            if (left < size && pq[left] < pq[smallest])
+            {
+                smallest = left;
+            }
+            //if right is smaller than root
+            if (right < size && pq[right] < pq[smallest])
+            {
+                smallest = right;
+            }
+            // swap nodes
+            if (smallest != index)
+            {
+                WordNode temp = pq[index];
+                pq[index] = pq[smallest];
+                pq[smallest] = temp;
+
+                MakeHeap(ref pq, size, smallest);
+            }
+        }
+        private bool IsMinHeap(List<WordNode> pq)
+        {
+            int size = pq.Count;
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (pq[i] < pq[(i - 1) / 2])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
